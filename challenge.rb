0d130fac1ee28b90d5185ddb42d69d2fcd747bfa
Hannes Schaletzky <hannes.schaletzky@gmx.de>
2021-06-11 15:54:58 +0200

    added challenge

---
+++ challenge.rb
@@ -0,0 +1,9 @@
+def amount_even_numbers(number)
+
+end
+
+# Challenge for Charles
+# return the count of even digits
+puts amount_even_numbers(1_654_327_842) == 5
+puts amount_even_numbers(8_263_842_368) == 8
+puts amount_even_numbers(172_844) == 4
