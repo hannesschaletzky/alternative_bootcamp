2d863b83cf423c5fc5b9a5849832542ea68d4ab0
Hannes Schaletzky <hannes.schaletzky@gmx.de>
2021-06-11 16:40:30 +0200

    challenge for ARIF

---
+++ challenge4.rb
@@ -0,0 +1,10 @@
+def regex(input)
+
+end
+
+# challenge for Arif
+# returns true if a string contains at the fourth position an 'A' &&
+# at the second last position a *
+puts regex('ajdAko3891j513bjkh1*3') == true
+puts regex('jdowiDduifenaio391o.-2"') == false
+puts regex('43kAwjdieoanlneioj*A') == true
