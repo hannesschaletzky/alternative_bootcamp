3cb78c0eb1b57ef68e2026dd29ca646c5f027ec2
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-06-11 16:20:37 +0200

    ex2

---
+++ challenge2.rb
@@ -0,0 +1,7 @@
+def sum_n_numbers(n,array)
+end
+
+# Challenge for Charles
+# return the sum of each n postitioned item in each array
+puts sum_n_numbers(1, [[8,4,5], [3,1,5], [1,2,3]]) == 14
+puts sum_n_numbers(2, [[9,5,4], [3,2,6], [2,4,4]]) == 11
--- challenge_two.rb
+++
@@ -1,7 +0,0 @@
-def sum_n_numbers(array)
-end
-
-# Challenge for Charles
-# return the count of even digits
-puts sum_n_numbers([8,4,5], [3,1,5], [1,2,3]) == 32
-puts sum_n_numbers([9,5,4], [3,2,6], [2,4,4]) == 39
