0e65e7ce53a5a80aad39059120d806c583dfd6de
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-06-14 16:24:05 +0200

    Added challenge 5

---
+++ challenge/challenge5.rb
@@ -0,0 +1,10 @@
+def count_names_with_letter(letter, names)
+    #add your code here
+end
+
+# challenge for Wagoney
+# returns the count of names that have the letter passed as argument
+
+puts count_names_with_letter('e', ['Sarah', 'Ryan', 'Andre']) == 1
+puts count_names_with_letter('a', ['Erik', 'Valentina', 'Hannah', 'Charles-Antoine']) == 3
+puts count_names_with_letter('i', ['Maxim', 'Hannes', 'Vincenzo', 'Vojtech']) == 2
