c20e469d25b305aeccd1d2c8a8275d9b08833463
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-06-16 15:58:21 +0200

    Added challenge 6

--- challenges/challenge6.rb
+++ challenges/challenge6.rb
@@ -1,35 +1,35 @@
 families = [
-  family_1 = {
+  {
     "dad" => 32,
     "mum" => 28,
     "daughter" => 5,
     "son" => 2
   },
-  family_2 = {
+  {
     "dad" => 45,
     "mum" => 43,
     "daughter" => 18,
     "son" => 16
   },
-  family_3 = {
+  {
     "dad" => 48,
     "mum" => 39,
     "daughter" => 21,
     "son" => 5
   },
-  family_4 = {
+  {
     "dad" => 35,
     "mum" => 36,
     "daughter" => 12,
     "son" => 18
   },
-  family_5 = {
+  {
     "dad" => 49,
     "mum" => 50,
     "daughter" => 25,
     "son" => 26
   },
-  family_6 = {
+  {
     "dad" => 55,
     "mum" => 39,
     "daughter" => 19,
@@ -52,9 +52,9 @@ end
 
 # Return the following (to simplify all families have a mum,dad, daughter and son)
 # return the sum of all ages from all families
-puts sum_all(families) == 677
+puts sum_all(families) == 688
 # return the sum of all male ages from all families
-puts sum_males(families) == 339
+puts sum_males(families) == 353
 # return the average age difference between daughter and son (two decimals rounded)
 puts avg_age_diff_daugther_son(families) == 5.17
 # puts vowels_count("aperol") == 3
