8078caba75faa765a77810bfade6075f8238f8e7
Hannes Schaletzky <hannes.schaletzky@gmx.de>
2021-06-18 16:41:14 +0200

    adjusted tests to eq

--- 1624025261_Hannes_1/challenge_spec.rb
+++ 1624025261_Hannes_1/challenge_spec.rb
@@ -4,21 +4,21 @@ require_relative 'challenge'
 
 describe 'decimal_to_binary' do
   it 'should return the binary for 105' do
-    expect(decimal_to_binary(105)).to be '1101001'
+    expect(decimal_to_binary(105)).to eq('1101001')
   end
   it 'should return the binary for 3' do
-    expect(decimal_to_binary(3)).to be '11'
+    expect(decimal_to_binary(3)).to eq('11')
   end
   it 'should return the binary for 607' do
-    expect(decimal_to_binary(607)).to be '1001011111'
+    expect(decimal_to_binary(607)).to eq('1001011111')
   end
   it 'should return the binary for 532329103' do
-    expect(decimal_to_binary(532329103)).to be '11111101110101011001010001111'
+    expect(decimal_to_binary(532329103)).to eq('11111101110101011001010001111')
   end
   it 'should return the binary for -83' do
-    expect(decimal_to_binary(-83)).to be '-1010011'
+    expect(decimal_to_binary(-83)).to eq('-1010011')
   end
   it 'should return the binary for 0' do
-    expect(decimal_to_binary(0)).to be '0'
+    expect(decimal_to_binary(0)).to eq('0')
   end
 end
