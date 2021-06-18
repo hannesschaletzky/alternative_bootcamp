242722d25196e0511571d4f14a6383eec56d59c9
Hannes Schaletzky <hannes.schaletzky@gmx.de>
2021-06-18 10:53:23 +0200

    init template script

---
+++ create_challenge.rb
@@ -0,0 +1,21 @@
+require 'FileUtils'
+
+# get folders
+folders = Dir.glob('*').select { |f| File.directory? f }
+# select challenge folders
+f_prefix = 'challenge_'
+chal_folders = folders.select { |f| f.include?(f_prefix) }
+chal_folders.sort!
+puts chal_folders
+# get highest number
+max_num = 0
+max_num = chal_folders.last.gsub(f_prefix, '').to_i unless chal_folders.empty?
+# insert leading zeros
+name = "#{f_prefix}#{max_num + 1}"
+name = "#{f_prefix}0#{max_num + 1}" if max_num < 999
+name = "#{f_prefix}00#{max_num + 1}" if max_num < 99
+name = "#{f_prefix}000#{max_num + 1}" if max_num < 9
+# create folder
+Dir.mkdir(name)
+# copy template to new challenge
+FileUtils.cp_r('template/.', "#{name}/")
