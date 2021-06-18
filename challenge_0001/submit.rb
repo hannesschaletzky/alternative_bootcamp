4b126671999c75fe1783324ecef61cc6e4cf1117
Hannes Schaletzky <hannes.schaletzky@gmx.de>
2021-06-18 15:13:06 +0200

    deleted challenges

--- challenge_0001/submit.rb
+++
@@ -1,8 +0,0 @@
-require 'fileutils'
-# get githubname and add it in the filepath
-name = `git config user.name`.chomp.gsub(' ', '')
-challenge_path = "#{File.dirname(__FILE__)}/challenge.rb"
-# save in the solution folder
-FileUtils.cp(challenge_path, "#{File.dirname(__FILE__)}/solutions/#{name}.rb")
-# revert changes in challenge.rb
-system("git restore #{challenge_path}")
