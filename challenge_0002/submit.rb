c44cc3a9352f7de83eef1e515b8db9fb91670af9
Hannes Schaletzky <hannes.schaletzky@gmx.de>
2021-06-18 12:05:51 +0200

    added automated push

--- challenge_0002/submit.rb
+++ challenge_0002/submit.rb
@@ -3,6 +3,12 @@ require 'fileutils'
 name = `git config user.name`.chomp.gsub(' ', '')
 challenge_path = "#{File.dirname(__FILE__)}/challenge.rb"
 # save in the solution folder
-FileUtils.cp(challenge_path, "#{File.dirname(__FILE__)}/solutions/#{name}.rb")
+solution_file = "#{File.dirname(__FILE__)}/solutions/#{name}.rb"
+FileUtils.cp(challenge_path, solution_file)
 # revert changes in challenge.rb
 system("git restore #{challenge_path}")
+
+# commit & push solution
+system("git add #{solution_file}")
+system('git commit -m "added solution"')
+system('git push origin master')
