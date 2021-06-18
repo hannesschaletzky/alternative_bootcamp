ced375dbabe10c20734425ae45d7f74a97811637
Hannes Schaletzky <hannes.schaletzky@gmx.de>
2021-06-18 12:22:28 +0200

    added submit functionality

--- template/submit.rb
+++ template/submit.rb
@@ -1,10 +1,27 @@
 require 'fileutils'
+require 'rspec'
+
+cur_dir = File.dirname(__FILE__)
+
+# verify successful tests
+spec_path = "#{cur_dir}/challenge_spec.rb"
+result = RSpec::Core::Runner.run([spec_path])
+if result == 1
+  puts "Your tests did not pass!"
+  puts "You cannot submit your solution..."
+  puts ""
+  return
+end
+puts "Your tests passed!"
+puts "Congrats"
+puts "(Insert GitHub SSH key if necessary)"
+puts ""
 
 # get githubname and add it in the filepath
 name = `git config user.name`.chomp.gsub(' ', '')
-challenge_path = "#{File.dirname(__FILE__)}/challenge.rb"
+challenge_path = "#{cur_dir}/challenge.rb"
 # save in the solution folder
-solution_file = "#{File.dirname(__FILE__)}/solutions/#{name}.rb"
+solution_file = "#{cur_dir}/solutions/#{name}.rb"
 FileUtils.cp(challenge_path, solution_file)
 # revert changes in challenge.rb
 system("git restore #{challenge_path}")
