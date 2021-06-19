b9b6043b03d3d6c20db7c2cf2000dfdf57b8ae17
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-06-19 13:41:09 +0200

    refactor

--- 1624025261_Hannes_1/challenge.rb
+++ 1624025261_Hannes_1/challenge.rb
@@ -4,5 +4,18 @@
 # specify the method signature (incl. parameters)
 
 def decimal_to_binary(num)
-
+  negative = false
+  negative = true if num.negative?
+  num = num.abs
+  binary = [num]
+  while num > 1
+    num /= 2
+    binary << num
+  end
+  "#{negative ? '-' : ''}#{arr.map { |x| x % 2 }.reverse.join}"
 end
+
+# p decimal_to_binary(0)
+# p decimal_to_binary(1)
+# p decimal_to_binary(2)
+# p decimal_to_binary(294)
