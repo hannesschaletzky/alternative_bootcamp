9ba07086564b2523c6d1d4431ff2cf62286d999a
Hannah Eichelsdoerfer <hannaheichelsdoerfer@gmail.com>
2021-06-23 10:08:26 +0200

    added solution

---
+++ 1624189427_difference/solutions/HannahEichelsdoerfer.rb
@@ -0,0 +1,11 @@
+# Please return the elements that are in a but not in b
+
+def difference_check(a, b)
+  (a-b)
+end
+
+# puts difference_check(['Hannah', 'Nate'], ['Hannah'])
+
+# Testing out:
+# (a && b).any?
+# a.any? { |item| b.include?(item) }
